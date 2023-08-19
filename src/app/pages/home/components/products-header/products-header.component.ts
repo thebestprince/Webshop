a40a4d7a527b4acad0f8e9453b7c557b96883198
Dwayne Reed <dreedjr1992@gmail.com>
2023-08-18 23:52:35 -0500

    Columns-Updated

--- src/app/pages/home/components/products-header/products-header.component.ts
+++ src/app/pages/home/components/products-header/products-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter,OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-products-header',
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   
 })
 export class ProductsHeaderComponent implements OnInit {
-  sort= 'desc';
+  @Output() columnsCountChange = new EventEmitter<number>();
+  sort = 'desc';
   itemsShowCount = 12
 
   constructor() { }
@@ -18,7 +19,11 @@ export class ProductsHeaderComponent implements OnInit {
   }
   
   onItemsUpdated(count: number): void {
-    this.itemsShowCount =count;
+    this.itemsShowCount = count;
+
+  }
+  onColumnsUpdated(colsNum: number): void {
+    this.columnsCountChange.emit(colsNum)
 
   }
 }
