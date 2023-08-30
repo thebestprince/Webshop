50c69888620ff759ff5464dc68fa38c6cbe5aa4c
Dwayne Reed <dreedjr1992@gmail.com>
2023-08-29 20:23:53 -0500

    categories

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   cols = 3;
+  category: string | undefined;
   constructor() { }
 
   ngOnInit(): void {
@@ -13,6 +14,10 @@ export class HomeComponent implements OnInit {
 
   onColumnsCountChange(colsNum: number): void {
      this.cols = colsNum;
+  }
+
+  onShowCategory(newCategory: string): void {
+    this.category = newCategory;
   }
 
 }
